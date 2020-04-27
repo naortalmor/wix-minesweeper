9c26fe2b5d0a6937f9ae251afc702c8966047d2e
Naor Talmor <naortalmornew@gmail.com>
2020-04-27 19:07:57 +0300

    build

---
+++ build/precache-manifest.0ba253a8643e86ab6c2224730800082b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59acbedbfd863413b0c9bad432af1332",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2a0b108b3570d411c6a",
+    "url": "/static/css/main.8a11ee1c.chunk.css"
+  },
+  {
+    "revision": "b3455fcec8dfcea9d70e",
+    "url": "/static/js/2.09796335.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.09796335.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2a0b108b3570d411c6a",
+    "url": "/static/js/main.6533d305.chunk.js"
+  },
+  {
+    "revision": "1e814049eb6d0e6e4b59",
+    "url": "/static/js/runtime-main.5f3b2643.js"
+  }
+]);
